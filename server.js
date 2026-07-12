2cddf03706d741b59b18c3cbe65333244c9f9704
Prem Kumar <premkumarjh231@gmail.com>
2026-07-12 11:05:21 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static(__dirname));
+
+io.on("connection", (socket) => {
+  console.log("User connected");
+
+  socket.on("chat-message", (data) => {
+    io.emit("chat-message", data);
+  });
+});
+
+server.listen(3000, () => {
+  console.log("Server running at http://localhost:3000");
+});
